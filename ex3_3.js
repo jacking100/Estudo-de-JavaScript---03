5558633127586ee942870d48df4e55eb8b10c7b7
jacking100 <jacking100@gmail.com>
2025-03-21 12:18:17 -0300

    first commit

---
+++ ex3_3.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")()
+const salario = Number(prompt("Salário R$: "))
+const tempo = Number(prompt("Tempo (anos):  "))
+const quadrienios = Math.floor(tempo/4)
+const acrescimo = salario * quadrienios /100
+
+console.log(`Quadrienios: ${quadrienios}`)
+console.log(`Salário Final R$: ${(salario + acrescimo).toFixed(2)}`)
+
+
